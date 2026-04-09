24e242c75ab79348d613417353ab63889d9c7e08
Etay Politi <etaypoliti@MacBook-Air-sl-Etay.local>
2026-04-09 18:13:07 +0300

    add execution time and pages count

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from crawler.crawler import crawl
-import os
+import os 
+import time
 from dotenv import load_dotenv
 
 
 def main():
     load_dotenv()
 
-    url = os.getenv("START_URL")
+    url = os.getenv("START_URL") 
+    start = time.time()
 
-    links = crawl(url)
-    print(links)
+    links = crawl(url) 
+    end = time.time()
+    print(links) 
+    print("Pages scanned:", len(links), "(limit:", MAX_PAGES, ")")
+    print("Time taken:", round(end - start, 2), "seconds")
 
 
 if __name__ == "__main__":
